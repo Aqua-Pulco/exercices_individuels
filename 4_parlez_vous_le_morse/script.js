511d1ce2cbac3aa0422753f28cd207a02f355c4d
AquaPulco <iris.yolal@gmail.com>
2025-07-11 13:35:29 +0200

    tab de lettres morses from morse word

--- 4_parlez_vous_le_morse/script.js
+++ 4_parlez_vous_le_morse/script.js
@@ -1,32 +1,45 @@
 import latinToMorse from './annexe1.js';
-//import morseToLatin from './annexe2.js';
-
-let latincharacterTab = [];
+import morseToLatin from './annexe2.js';
 
 function getLatinCharacterList(texte) {
-    console.log(texte);
+    // console.log(texte);
     let tab = texte.split("");
-
-    return tab
+    return tab;
 }
-latincharacterTab = getLatinCharacterList("Hello, Paris");
-console.log(latincharacterTab);
+
+console.log(getLatinCharacterList("Hello, Paris")); // je peux split une string en array
 
 
-function translateLatinCharacter(char) {
+function translateLatinCharacter(char) { //je peux aller chercher une correspondance dans un dictionnaire pour 1 lettre
     char = char.toUpperCase()
     if (char in latinToMorse === false) {
-       return char 
+        return char;
     }
     return latinToMorse[char];
 }
 
-function encode (texte){
-  let tab1 =  getLatinCharacterList(texte);
-  let tab2 = [];
-    for (const el of tab1) {  
-      tab2 = `${tab2} ${translateLatinCharacter(el)}`;        
+function encode(texte) { // je peux transformer un texte en tableau, traduire chaque el et reconstituer une string
+    let tab1 = getLatinCharacterList(texte);
+    let tab2 = [];
+    for (const el of tab1) {
+        tab2 = `${tab2} ${translateLatinCharacter(el)}`;
     }
     return tab2;
 }
 console.log(encode('lalala'));
+
+function getMorse(text) {
+    console.log(text)
+   let morse =  encode(`${text}`)
+   console.log(morse);
+}
+getMorse('test');
+
+// string = tableau
+
+function decode(morse){
+    let morseLetter = morse.split(" ");
+    console.log(morseLetter);
+   }
+
+decode('- . ... -');

030facbd5bc105c4abdd2508fca8ed7f8cb3112c
Iris Aqua <iris.yolal@gmail.com>
2025-09-21 12:48:14 +0200

    sapin part1.1

--- 9_sapin/script.js
+++ 9_sapin/script.js
@@ -1,5 +1,25 @@
+// Le backslash \ n’est pas “un caractère normal” dans une chaîne
 
-const sapin = [
+// Dans un littéral de chaîne ("...", '...', `...`), \ introduit une séquence d’échappement :
+
+// \" signifie : mets un guillemet " dans la chaîne (et ne termine pas la chaîne).
+// \\ signifie : mets un backslash \ dans la chaîne.
+// \n, \t… sont des sauts de ligne, tabulations, etc.
+// \xHH attend deux hexadécimaux (ex. \x41 = A).
+// \uXXXX et \u{...} sont des escapes Unicode.
+
+// Conséquence immédiate : écrire un \ “brut” à la fin d’une chaîne n’est pas autorisé.
+// Il va essayer “d’échapper” le guillemet fermant, ce qui rend le littéral invalide.
+// \" n’ajoute pas un backslash, mais un guillemet dans la chaîne
+// \x commence une seq hexaD. et exige deux chiffres derrière > cela crée une erreur de syntaxe
+
+// ctrl + L selectionne ligne
+// alt + up deplace ligne
+
+
+///////////// --ETAPE 1-- \\\\\\\\\\\\\\
+
+const sapinUn = [
     "    +",
     "   /|\\",
     "  /*|*\\",
@@ -14,17 +34,29 @@ function afficherPointeSapin(hauteur) {
         return
     }
     else
-    for  (let i = 0; i < sapin.length; i++) {
-        console.log(sapin[i]);
-        if (i === hauteur) {
-            return
+        for (let i = 0; i < sapinUn.length; i++) {
+            console.log(sapinUn[i]);
+            if (i === hauteur) {
+                return
+            }
         }
-
-    }
 }
 
-
-afficherPointeSapin(4);
-
+afficherPointeSapin(5);
 
 
+///////////// --1.1 -ligne d'* -- \\\\\\\\\\\\\\
+
+function afficherEtoiles(n) {
+
+    let text = [];
+
+    for (i = 1; i <= n; i++) {
+        text += '*';    
+    }
+    console.log(text.trim());
+    return
+}
+
+afficherEtoiles(2)
+afficherEtoiles(5)

5edc861458a87dd0c44b90fe936f4887d456020f
Iris Aqua <iris.yolal@gmail.com>
2025-09-21 12:17:21 +0200

    sapin part1 fixed + comments

--- 9_sapin/script.js
+++ 9_sapin/script.js
@@ -7,23 +7,24 @@ const sapin = [
     "/***|***\\x"
 ];
 
-
 function afficherPointeSapin(hauteur) {
 
-    if (!hauteur || hauteur < 1 || hauteur > 4) {
-        console.log('veuillez renseigner un chiffre entre 1 et 4');
+    if (isNaN(hauteur) || hauteur < 0 || hauteur > 4) {
+        console.log('veuillez renseigner un chiffre entre 0 et 4');
+        return
     }
     else
-        for (let i = 0; i < sapin.length; i++) {
-            hauteur = hauteur - 1;
-            console.log(sapin[i]);
-            if (i === hauteur) {
-                return
-            }
-
+    for  (let i = 0; i < sapin.length; i++) {
+        console.log(sapin[i]);
+        if (i === hauteur) {
+            return
         }
+
+    }
 }
 
 
 afficherPointeSapin(4);
+
+
 
